fa32e4001c36811af431bbb26d5ed6b385b30ea7
Mansur <mansur.khusnutdinov@underhood.dev>
2024-02-14 00:52:17 +0300

    TEA-20 - in progress 3

--- packages/client/src/pages/Leaderboard/BoardItem/index.tsx
+++ packages/client/src/pages/Leaderboard/BoardItem/index.tsx
@@ -1,18 +1,26 @@
 import styles from './styles.module.scss'
 
 type Props = {
-  position: number;
-  image: string;
-  name: string;
-  score: number;
+  position: number
+  photo: string
+  icon: string
+  name: string
+  score: number
 }
 
 const BoardItem = (props: Props) => {
   return (
     <li className={styles.wrapper}>
       <div className={styles.iconArea}>
-        <img src={props.image} className={styles.icon}></img>
+        <img src={props.icon} className={styles.icon}></img>
       </div>
+
+      <div className={styles.personalArea}>
+        <img src={props.photo} className={styles.icon}></img>
+        <span className={styles.name}>{props.name}</span>
+      </div>
+
+      <div className={styles.scoreArea}>{props.score}</div>
     </li>
   )
 }
--- packages/client/src/pages/Leaderboard/index.tsx
+++ packages/client/src/pages/Leaderboard/index.tsx
@@ -1,18 +1,77 @@
 import styles from './styles.module.scss'
 import BoardItem from './BoardItem/index'
+import { useEffect, useState } from 'react'
+import { logDOM } from '@testing-library/react'
+
+// моковые данные
+const leaders = [
+  {
+    position: 1,
+    name: 'Michael',
+    score: 15,
+  },
+  {
+    position: 2,
+    name: 'Alex',
+    score: 12,
+  },
+  {
+    position: 3,
+    name: 'Nikita',
+    score: 10,
+  },
+  {
+    position: 4,
+    name: 'Dima',
+    score: 5,
+  },
+]
+const server = {
+  getLeaders() {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(leaders), 150)
+    })
+  },
+}
+
+interface ILeader {
+  position: number
+  name: string
+  score: number
+  photo?: string
+}
 
 const Leaderboard = () => {
+  const [list, setList] = useState<ILeader[]>([])
 
-  return <div className={styles.wrapper}>
-    <div className={styles.backgroundWrapper}>
-      <button className={styles.button}>На главную</button>
-      <h1 className={styles.header}>Рейтинг игроков</h1>
+  useEffect(() => {
+    server.getLeaders().then(response => setList(response as ILeader[]))
+  }, [])
 
-      <ul className={styles.boardContainer}>
-        <BoardItem />
-      </ul>
+  const setIcon = () => {
+    return ''
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.backgroundWrapper}>
+        <button className={styles.button}>На главную</button>
+        <h1 className={styles.header}>Рейтинг игроков</h1>
+
+        <ul className={styles.boardContainer}>
+          {list.map(item => (
+            <BoardItem
+              position={item.position}
+              name={item.name}
+              photo={item.photo ? item.photo : ''}
+              icon={setIcon()}
+              score={item.score}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default Leaderboard
