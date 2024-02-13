83ad14e0827764029627d5da4645570d09a012cf
V0L40K <vol4ok94@gmail.com>
2024-02-13 21:22:05 +0600

    feat eslint

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,9 +1,9 @@
-import dotenv from 'dotenv'
 import cors from 'cors'
-dotenv.config()
-
+import dotenv from 'dotenv'
 import express from 'express'
-import { createClientAndConnect } from './db'
+import {createClientAndConnect} from './db'
+
+dotenv.config()
 
 const app = express()
 app.use(cors())
@@ -12,9 +12,9 @@ const port = Number(process.env.SERVER_PORT) || 3001
 createClientAndConnect()
 
 app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :)')
+	res.json('👋 Howdy from the server :)')
 })
 
 app.listen(port, () => {
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+	console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
 })
