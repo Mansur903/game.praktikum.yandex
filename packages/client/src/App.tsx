72278126a8f0ca29ff4f51fb7fd6400d14f42632
V0L40K <vol4ok94@gmail.com>
2024-02-13 21:51:10 +0600

    feat Router

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,7 @@
 import {useEffect} from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
+import FirstComponent from './components/firstComponent'
 
 const App = () => {
 	useEffect(() => {
@@ -12,7 +14,22 @@ const App = () => {
 
 		fetchServerData()
 	}, [])
-	return <div className='App'>Вот тут будет жить ваше приложение :)</div>
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path={'/'}
+						element={<FirstComponent />}
+					/>
+					<Route
+						path={'*'}
+						element={<div>error404</div>}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App
