feeb66d913b1d63378f98f1fa5edbe4d5727fe28
V0L40K <vol4ok94@gmail.com>
2024-02-14 08:31:06 +0600

    fix Test

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,14 +1,20 @@
-import App from './App'
-import { render, screen } from '@testing-library/react'
-
-const appContent = 'Вот тут будет жить ваше приложение :)'
+import {render, screen} from '@testing-library/react'
+import {MemoryRouter, Routes, Route} from 'react-router-dom'
 
 // @ts-ignore
-global.fetch = jest.fn(() =>
-  Promise.resolve({ json: () => Promise.resolve('hey') })
-)
+global.fetch = jest.fn(() => Promise.resolve({json: () => Promise.resolve('hey')}))
 
-test('Example test', async () => {
-  render(<App />)
-  expect(screen.getByText(appContent)).toBeDefined()
+test('renders error404 when path is unknown', () => {
+	render(
+		<MemoryRouter initialEntries={['/unknown']}>
+			<Routes>
+				<Route
+					path='*'
+					element={<div>error404</div>}
+				/>
+			</Routes>
+		</MemoryRouter>
+	)
+	const error404Element = screen.getByText(/error404/i)
+	expect(error404Element.innerHTML).toEqual('error404')
 })
