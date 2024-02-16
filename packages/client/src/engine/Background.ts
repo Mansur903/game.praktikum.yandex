465b7763e1eae106c536aa59e2ae34bb2862c9cb
Mihailov Denis <sir.karakulia@gmail.com>
2024-02-16 20:15:04 +0700

    feat: add rout and game start screen

---
+++ packages/client/src/engine/Background.ts
@@ -0,0 +1,8 @@
+import GameElement from './GameElement'
+import bg from './../assets/game/BG.png'
+export default class Background extends GameElement {
+	constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+		super(canvas, context)
+		this.sprite.src = bg
+	}
+}
