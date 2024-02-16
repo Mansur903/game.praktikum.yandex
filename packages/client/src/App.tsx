781f686c1918140cc7ffa65940ee0dba23129e40
Mihailov Denis <sir.karakulia@gmail.com>
2024-02-16 17:42:11 +0700

    fix: check master for rebase

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,7 +3,6 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
 import FirstComponent from './components/FirstComponent'
 
-<<<<<<< HEAD
 const App = () => {
 	useEffect(() => {
 		const fetchServerData = async () => {
@@ -31,20 +30,6 @@ const App = () => {
 			</BrowserRouter>
 		</div>
 	)
-=======
-function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-
-    fetchServerData()
-  }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
->>>>>>> 221fd63 (TEA-13 - final fix)
 }
 
 export default App
