6eaba84aa8bd515c5ec359f679e33984dc6e19d0
Mansur <mansur.khusnutdinov@underhood.dev>
2024-02-18 18:35:47 +0300

    TEA-17 - finished createTopicPage

--- packages/client/src/pages/Forum/CreateTopic/index.tsx
+++ packages/client/src/pages/Forum/CreateTopic/index.tsx
@@ -1,8 +1,34 @@
 import styles from './styles.module.scss'
 import createTopicIcon from '../../../assets/create-topic-icon.png'
 import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import {styled} from '@mui/material/styles'
 
 const CreateTopicPage = () => {
+	const StyledTextField = styled(TextField)(() => ({
+		'.MuiInputBase-root': {
+			borderRadius: '20px',
+			backgroundColor: '#f0f0f0',
+			marginTop: '10px'
+		},
+		'.MuiOutlinedInput-notchedOutline': {
+			borderColor: '#f0f0f0'
+		},
+		'.MuiInputBase-root:hover .MuiOutlinedInput-notchedOutline': {
+			borderColor: 'white'
+		}
+	}))
+
+	const StyledButton = styled(Button)(() => ({
+		color: '#f0f0f0',
+		borderRadius: '10px',
+		borderColor: '#f0f0f0',
+		'&:hover': {
+			borderColor: '#fff'
+		},
+		margin: '40px 20px 0 0'
+	}))
+
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.page}>
@@ -15,22 +41,35 @@ const CreateTopicPage = () => {
 				</div>
 
 				<form className={styles.page__form}>
-					<TextField
-						id='outlined-basic'
-						variant='outlined'
-						sx={{
-							'.MuiInputBase-root': {
-								borderRadius: '20px',
-								backgroundColor: '#f0f0f0'
-							},
-							'.MuiOutlinedInput-notchedOutline': {
-								borderColor: '#f0f0f0'
-							},
-							'.MuiInputBase-root:hover .MuiOutlinedInput-notchedOutline': {
-								borderColor: 'white'
-							}
-						}}
-					/>
+					<div className={styles.page__textFieldWrapper}>
+						<h2 className={styles.header}>Название топика (Максимальная длина: 80)</h2>
+						<StyledTextField
+							id='outlined-basic'
+							variant='outlined'
+							sx={{
+								width: '80%',
+								'.MuiInputBase-root': {
+									height: '50px'
+								}
+							}}
+						/>
+					</div>
+
+					<div className={styles.page__textFieldWrapper}>
+						<h2 className={styles.header}>Комментарий</h2>
+						<StyledTextField
+							id='outlined-multiline-static'
+							multiline
+							rows={4}
+							sx={{
+								width: '100%'
+							}}
+						/>
+					</div>
+
+					<div className={styles.page__buttonWrapper}>
+						<StyledButton variant='outlined'>Создать</StyledButton>
+					</div>
 				</form>
 			</div>
 		</div>
