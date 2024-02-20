bc26310b7cd6b85c0a400ca3cc2ce4327fbe56fe
Mansur <mansur.khusnutdinov@underhood.dev>
2024-02-20 21:29:38 +0300

    TEA-13

--- packages/client/src/pages/NotFound/404.tsx
+++ packages/client/src/pages/NotFound/404.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-
 import styles from './styles.module.scss'
 
 const NotFoundPage = () => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.contentBlock}>
-        <h1 className={styles.header}>404</h1>
-        <p className={styles.paragraph}>Упс... здесь ничего нет</p>
-        <button className={styles.button}>Вернуться на главную</button>
-      </div>
-    </div>
-  )
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.page}>
+				<h1 className={styles.page__header}>404</h1>
+				<p className={styles.page__paragraph}>Упс... здесь ничего нет</p>
+				<button className={styles.page__button}>Вернуться на главную</button>
+			</div>
+		</div>
+	)
 }
 
 export default NotFoundPage
