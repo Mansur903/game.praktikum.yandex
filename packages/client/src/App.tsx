1379f1775df892469819661fd238d3b0fbad638e
Mihailov Denis <sir.karakulia@gmail.com>
2024-02-21 21:42:20 +0700

    feat: ad game end state

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,9 @@
 import {useEffect} from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
+import Game from './pages/Game/Game'
 import MainPage from './pages/MainPage/MainPage'
 import NotFoundPage from './pages/NotFound/404'
-import Game from './pages/Game'
 import Leaderboard from './pages/Leaderboard'
 import Profile from './pages/Profile/Profile'
 
--- packages/client/src/engine/GameEngine.ts
+++ packages/client/src/engine/GameEngine.ts
@@ -1,4 +1,6 @@
+import {GameState} from '../types/enum/Game.enum'
 import Background from './Background'
+import constants from './constants'
 import UI from './UI'
 
 export default class GameEngine {
@@ -7,13 +9,27 @@ export default class GameEngine {
 	background
 	ui
 	frames = 0
+	state = GameState.START
 	constructor(canvas: HTMLCanvasElement) {
 		this.canvas = canvas
+		this.canvas.addEventListener('click', this.stateChange.bind(this))
 		const context = this.canvas.getContext('2d')
 		if (!context) throw Error('Error: missing context')
 		this.context = context
 		this.background = new Background(this.canvas, this.context)
 		this.ui = new UI(this.canvas, this.context)
+	}
+
+	stateChange() {
+		console.log('test', this.state)
+		switch (this.state) {
+			case GameState.START:
+				this.state = GameState.END
+				break
+			case GameState.END:
+				this.state = GameState.START
+				break
+		}
 	}
 
 	start() {
@@ -24,11 +40,10 @@ export default class GameEngine {
 	}
 
 	draw() {
-		//TODO: вынести цвет в переменную
-		this.context!.fillStyle = '#30c0df'
-		this.context!.fillRect(0, 0, this.canvas.width, this.canvas.height)
+		this.context.fillStyle = constants.color.sky
+		this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
 		this.background.draw()
-		this.ui.draw()
+		this.ui.draw(this.state)
 	}
 
 	update() {
