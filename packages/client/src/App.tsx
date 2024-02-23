46964b16bcae14a76cfaa110d52a2ac1455803cb
Mansur <mansur.khusnutdinov@underhood.dev>
2024-02-23 22:27:27 +0300

    TEA-33

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
-import {useEffect} from 'react'
+import React, {useEffect, useCallback} from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {FullScreen, useFullScreenHandle} from 'react-full-screen'
 import './App.css'
 import Register from './pages/Register/Register'
 import Game from './pages/Game/Game'
@@ -9,6 +10,15 @@ import Leaderboard from './pages/Leaderboard'
 import Profile from './pages/Profile/Profile'
 
 const App = () => {
+	const handle = useFullScreenHandle()
+
+	const toggleFullScreen = useCallback(
+		(e: React.KeyboardEvent) => {
+			if (e.key === 'f') handle.active ? handle.exit() : handle.enter()
+		},
+		[handle.active]
+	)
+
 	useEffect(() => {
 		const fetchServerData = async () => {
 			const url = `http://localhost:${__SERVER_PORT__}`
@@ -20,40 +30,45 @@ const App = () => {
 		fetchServerData()
 	}, [])
 	return (
-		<div className='App'>
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path={'/'}
-						element={<MainPage />}
-					/>
-					<Route
-						path={'/game'}
-						element={<Game />}
-					/>
-					<Route
-						path={'/profile'}
-						element={<Profile />}
-					/>
-					<Route
-						path={'/leaderboard'}
-						element={<Leaderboard />}
-					/>
-					<Route
-						path={'/signup'}
-						element={<Register />}
-					/>
-					<Route
-						path={'*'}
-						element={<NotFoundPage />}
-					/>
-					<Route
-						path={'/game'}
-						element={<Game />}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</div>
+		<FullScreen handle={handle}>
+			<div
+				className='App'
+				tabIndex={0}
+				onKeyDown={toggleFullScreen}>
+				<BrowserRouter>
+					<Routes>
+						<Route
+							path={'/'}
+							element={<MainPage />}
+						/>
+						<Route
+							path={'/game'}
+							element={<Game />}
+						/>
+						<Route
+							path={'/profile'}
+							element={<Profile />}
+						/>
+						<Route
+							path={'/leaderboard'}
+							element={<Leaderboard />}
+						/>
+						<Route
+							path={'/signup'}
+							element={<Register />}
+						/>
+						<Route
+							path={'*'}
+							element={<NotFoundPage />}
+						/>
+						<Route
+							path={'/game'}
+							element={<Game />}
+						/>
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</FullScreen>
 	)
 }
 
