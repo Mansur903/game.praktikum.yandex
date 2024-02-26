f79d76ec0cbb0371bf6309242ede2b2c674ab20f
Mihailov Denis <sir.karakulia@gmail.com>
2024-02-26 20:27:05 +0700

    feat: add event change game state

--- packages/client/src/engine/GameEngine.ts
+++ packages/client/src/engine/GameEngine.ts
@@ -3,14 +3,26 @@ import Background from './Background'
 import constants from './constants'
 import UI from './UI'
 
-export default class GameEngine {
+/* Класс GameEngine в TypeScript управляет состоянием игры, обрабатывает взаимодействие с пользователем
+и управляет игровым циклом простого игрового приложения. */
+export default class GameEngine extends EventTarget {
 	canvas: HTMLCanvasElement
 	context: CanvasRenderingContext2D
 	background
 	ui
 	frames = 0
+	/**Текущее состояние игры. Может пребывать в трех различных состояниях:
+	 * START - стартовый экран игры
+	 * PLAY - игра идёт, птичка прыгает
+	 * END - экран поражения
+	 */
 	state = GameState.START
+	/**
+	 * Количество последних набранных очков. изначальное состояние равно 0.
+	 */
+	point = 0
 	constructor(canvas: HTMLCanvasElement) {
+		super()
 		this.canvas = canvas
 		this.canvas.addEventListener('click', this.stateChange.bind(this))
 		const context = this.canvas.getContext('2d')
@@ -18,19 +30,41 @@ export default class GameEngine {
 		this.context = context
 		this.background = new Background(this.canvas, this.context)
 		this.ui = new UI(this.canvas, this.context)
+		setInterval(() => {
+			this.update()
+			this.frames++
+		}, 50)
 	}
 
+	/**
+	 * Функция stateChange переключает между двумя состояниями игры и увеличивает количество очков, а затем
+	 * отправляет пользовательское событие с текущим состоянием и точкой.
+	 */
 	stateChange() {
 		switch (this.state) {
 			case GameState.START:
+				this.point++
 				this.state = GameState.END
 				break
 			case GameState.END:
+				this.point++
 				this.state = GameState.START
 				break
 		}
+		this.dispatchEvent(
+			new CustomEvent('changeState', {
+				detail: {
+					currState: this.state,
+					currPoint: this.point
+				}
+			})
+		)
 	}
 
+	/**
+	 * Функция start использует requestAnimationFrame для непрерывного вызова метода gameLoop и самого себя
+	 * для плавной анимации в TypeScript.
+	 */
 	start() {
 		requestAnimationFrame(() => {
 			this.gameLoop()
@@ -38,6 +72,10 @@ export default class GameEngine {
 		})
 	}
 
+	/**
+	 * Функция draw устанавливает цвет фона холста, рисует фон, а затем рисует пользовательский интерфейс
+	 * на основе текущего состояния.
+	 */
 	draw() {
 		this.context.fillStyle = constants.color.sky
 		this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
@@ -45,13 +83,17 @@ export default class GameEngine {
 		this.ui.draw(this.state)
 	}
 
+	/**
+	 * Функция обновления вызывает метод обновления объекта пользовательского интерфейса с параметром
+	 * кадров.
+	 */
 	update() {
 		this.ui.update(this.frames)
 	}
-
+	/**
+	 * Функция gameLoop обновляет состояние игры, рисует игру и увеличивает количество кадров.
+	 */
 	gameLoop() {
-		this.update()
 		this.draw()
-		this.frames++
 	}
 }
--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -1,19 +1,33 @@
-import {FC, useEffect, useRef} from 'react'
+import {FC, useEffect, useRef, useState} from 'react'
 import GameEngine from '../../engine/GameEngine'
 import styles from './Game.module.scss'
+import {Typography} from '@mui/material'
+
+const isCustomEvent = (event: Event): event is CustomEvent => {
+	return 'detail' in event
+}
 
 const Game: FC = () => {
 	const ref = useRef<HTMLCanvasElement>(null)
-
+	const [somePoint, setSomePoint] = useState<number>(0)
 	useEffect(() => {
 		if (ref.current) {
+			const eventHandler = (event: Event) => {
+				if (!isCustomEvent(event)) throw Error('Not custom event')
+				console.info(event.detail)
+				setSomePoint(event.detail.currPoint)
+			}
 			const game = new GameEngine(ref.current)
+			game.addEventListener('changeState', eventHandler)
 			game.start()
+			return () => game.removeEventListener('changeState', eventHandler)
 		}
+		return
 	}, [])
 
 	return (
 		<div className={styles.wrapper}>
+			<Typography variant='body1'>Текущий счет: {somePoint}</Typography>
 			<canvas
 				ref={ref}
 				width={276}
