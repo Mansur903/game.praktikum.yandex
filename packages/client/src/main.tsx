bff6a4c115723727872b5dafaf9a6e052747bece
Mansur <mansur.khusnutdinov@underhood.dev>
2024-03-03 14:54:36 +0300

    TEA-34

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,5 +1,14 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
+import store from './store'
+import {Provider} from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+)
