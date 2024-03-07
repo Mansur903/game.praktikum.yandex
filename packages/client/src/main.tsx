aed7cf9f253c915314105abbf96709be1c214ed8
Mansur <mansur.khusnutdinov@underhood.dev>
2024-03-07 18:05:05 +0300

    TEA-30

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -5,6 +5,26 @@ import './index.scss'
 import store from './store'
 import {Provider} from 'react-redux'
 
+function startServiceWorker() {
+	if ('serviceWorker' in navigator) {
+		window.addEventListener('load', () => {
+			navigator.serviceWorker
+				.register('../sw.ts')
+				.then((registration) => {
+					console.log(
+						'ServiceWorker registration successful with scope: ',
+						registration.scope
+					)
+				})
+				.catch((error) => {
+					console.log('ServiceWorker registration failed: ', error)
+				})
+		})
+	}
+}
+
+startServiceWorker()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<Provider store={store}>
