3fec99c499edc5f40ef2613ca4136951a4fcfeed
Mihailov Denis <sir.karakulia@gmail.com>
2024-03-17 16:52:47 +0700

    fix: delete comment

--- packages/client/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ packages/client/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -14,23 +14,16 @@ class ErrorBoundary extends Component<PropsType, StateType> {
 	}
 
 	static getDerivedStateFromError() {
-		// Update state so the next render will show the fallback UI.
 		return {hasError: true}
 	}
 
 	componentDidCatch(error: Error, info: ErrorInfo) {
-		// Example "componentStack":
-		//   in ComponentThatThrows (created by App)
-		//   in ErrorBoundary (created by App)
-		//   in div (created by App)
-		//   in App
 		console.error(error, info.componentStack)
 	}
 
 	render() {
 		const {children, fallback} = this.props
 		if (this.state.hasError) {
-			// You can render any custom fallback UI
 			return fallback ? (
 				fallback()
 			) : (
