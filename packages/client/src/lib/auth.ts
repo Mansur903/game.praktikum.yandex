19255c172b204c18f1f5e7bec5b4d5e49c408b02
Хатия Хахутаишвили <68025454+xatepk@users.noreply.github.com>
2024-04-07 19:00:18 +0300

    [TEA-38] OAuth authentication has been added to the project (#26)
    * [TEA-38] OAuth authentication has been added to the project
    
    * [TEA-38] fix
    
    * [TEA-38] fix
    
    * [TEA-38] fix

---
+++ packages/client/src/lib/auth.ts
@@ -0,0 +1,52 @@
+import {callJsApi} from './utils'
+import {AppError, AppErrorCode} from './error'
+import {OAUTH_BASE_URL, OAUTH_REDIRECT_URI} from '../config/api'
+
+enum YandexAuthErrorName {
+	NotLoggedIn = 'AuthErrorCode::ERROR_NOT_LOGGED_IN'
+}
+
+type PSUIDPayload = {
+	psuid: string
+}
+
+type JWTPayload = {
+	iss: string
+	iat: number
+	jti: string
+	exp: number
+}
+
+export type YandexAuthPSUIDInfo = {
+	payload: JWTPayload & PSUIDPayload
+	code: string
+}
+
+export async function jsApiIdentify(clientID: string): Promise<YandexAuthPSUIDInfo> {
+	try {
+		return await callJsApi({
+			name: 'window.yandex.app.auth.identify',
+			args: [clientID],
+			scope: window.yandex?.app?.auth,
+			method: 'identify'
+		})
+	} catch (err) {
+		if (err instanceof AppError) {
+			if (err.name === YandexAuthErrorName.NotLoggedIn) {
+				throw new AppError(AppErrorCode.JsApiCancelled, 'User not logged.')
+			}
+		}
+
+		throw err
+	}
+}
+
+export function redirectToOauthAuthorize(clientID: string): void {
+	const search = new URLSearchParams()
+
+	search.append('client_id', clientID)
+	search.append('redirect_uri', OAUTH_REDIRECT_URI)
+	search.append('response_type', 'code')
+
+	window.location.href = `${OAUTH_BASE_URL}?${search.toString()}`
+}
